6dd4e3c72038d325ddbcdc5d92001a7ab1741c90
gary443211 <124863128+gary443211@users.noreply.github.com>
2023-12-28 01:41:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 import cv2
 import math 
 # start webcam
-cap = cv2.VideoCapture(4)
+cap = cv2.VideoCapture(6)
 cap.set(3, 640)
 cap.set(4, 480)
 
@@ -48,7 +48,16 @@ while True:
             thickness = 2
 
             cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+            
+    # Calculate FPS
+    current_time = time()
+    fps = 1 / (current_time - prev_time)
+    prev_time = current_time
 
+    # Display confidence and FPS text
+    cv2.putText(img, f'Confidence: {confidence}', (10, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    cv2.putText(img, f'FPS: {fps:.2f}', (10, 390), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+    
     cv2.imshow('realsence', img)
     if cv2.waitKey(1) == ord('q'):
         break
