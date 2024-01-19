80d37bd61c6c7c1b9d2709aed373f95a9879105d
gary443211 <124863128+gary443211@users.noreply.github.com>
2024-01-20 03:55:21 +0800

    Create main3.py

---
+++ main3.py
@@ -0,0 +1,108 @@
+from ultralytics import YOLO
+import cv2
+import math
+import numpy as np
+import pyrealsense2 as rs
+from time import time
+
+# Create a RealSense pipeline
+pipeline = rs.pipeline()
+config = rs.config()
+config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+pipeline.start(config)
+
+# Model
+model = YOLO("onboard_cam.pt")
+
+# Object classes
+classNames = ("plant")
+
+# Initialize variables for FPS calculation
+prev_time = 0
+fps = 0
+
+# Set the desired window resolution
+window_width = 640
+window_height = 360
+
+# Create an OpenCV window
+cv2.namedWindow('RealSense YOLO', cv2.WINDOW_NORMAL)
+cv2.resizeWindow('RealSense YOLO', window_width, window_height)
+
+# test
+frames = pipeline.wait_for_frames()
+print("Number of frames:", len(frames))
+depth_frame = frames.get_depth_frame()
+if depth_frame:
+    Depth = depth_frame.get_distance(320, 180)
+    print("central depth --->", Depth)
+else:
+    print("Depth frame is None")
+
+
+# while True:
+#     # Wait for a RealSense frame
+#     frames = pipeline.wait_for_frames()
+#     color_frame = frames.get_color_frame()
+#     depth_frame = frames.get_depth_frame()
+
+#     # Convert RealSense frame to OpenCV format
+#     img = np.asanyarray(color_frame.get_data())
+
+#     # Resize the image to match the window resolution
+#     img = cv2.resize(img, (window_width, window_height))
+
+#     results = model(img, stream=True)
+
+#     # Initialize confidence variable
+#     confidence = 0
+
+#     # Coordinates
+#     for r in results:
+#         boxes = r.boxes
+
+#         for box in boxes:
+#             # Bounding box
+#             x1, y1, x2, y2 = box.xyxy[0]
+#             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)  # Convert to int values
+
+#             # Draw bounding box on the frame
+#             cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 2)
+
+#             # Confidence
+#             confidence = math.ceil((box.conf[0] * 100)) / 100
+#             print("Confidence --->", confidence)
+
+#             # Class name
+#             cls = int(box.cls[0])
+#             print("Class name -->", classNames[cls])
+
+#             # Get depth of the center of the box
+#             d1, d2 = int((x1+x2)/2), int((y1+y2)/2)
+#             Depth = depth_frame.get_distance(int(d1),int(d2))  # by default realsense returns distance in meters 
+
+#             # Object details
+#             org = (x1, y1)
+#             font = cv2.FONT_HERSHEY_SIMPLEX
+#             fontScale = 1
+#             color = (255, 0, 0)
+#             thickness = 1
+
+#             cv2.putText(img, classNames[cls], org, font, fontScale, color, thickness)
+#             cv2.putText(img, Depth, org, font, fontScale, color, thickness)
+
+#     # Calculate FPS
+#     current_time = time()
+#     fps = 1 / (current_time - prev_time)
+#     prev_time = current_time
+
+#     # Display confidence and FPS text
+#     cv2.putText(img, f'Confidence: {confidence}', (10, 310), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+#     cv2.putText(img, f'FPS: {fps:.2f}', (10, 350), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+
+#     cv2.imshow('RealSense YOLO', img)
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+
+# pipeline.stop()
+# cv2.destroyAllWindows()
