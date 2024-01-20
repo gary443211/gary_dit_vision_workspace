3f4d80d2ecf6301033864122644220830ff6e5c9
gary443211 <124863128+gary443211@users.noreply.github.com>
2024-01-20 18:29:17 +0800

    Update main3.py

--- main3.py
+++ main3.py
@@ -9,6 +9,7 @@ from time import time
 pipeline = rs.pipeline()
 config = rs.config()
 config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 pipeline.start(config)
 
 # Model
